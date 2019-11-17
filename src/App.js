cd2bfc83b82422ef063fbb990d9ba1bc1ce174dc
Gerardo J Enrique <geraenrique97@hotmail.com>
2019-11-17 18:13:45 -0300

    hotfix the validate function

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 
 function Alert(props) {
   return (
-    <div id="Result" className={props.valid? 'alert alert-success': 'alert alert-danger'} role="alert">
-      {props.valid? 'Valid string': 'Invalid string'}
+    <div id="Result" className={ props.valid? 'shadow alert alert-success': 'shadow alert alert-danger'} role="alert">
+      {props.valid? 'Valid string!': 'Invalid string!'}
     </div>
   )
 }
@@ -19,42 +19,59 @@ class App extends React.Component {
 
   }
 
+  countOccurrences(values) {
+    let occurrences = {}
+    values.forEach( element => {
+      occurrences[element] != undefined? occurrences[element] = occurrences[element] + 1: occurrences[element] = 1
+    });
+    return occurrences
+  }
 
   validateString(){
     let valid
     let string = this.state.valueString;
-    let occurrences = [];
-    let char
-    while (string) {
-      char = string[0];
-      occurrences[char] = string.match( RegExp(char, 'gi')).length;
-      string = string.replace(RegExp(char, 'gi'), '');
-    }
-    console.log(occurrences);
-    let count = {}
-    Object.values(occurrences).forEach( element => {
-      count[element] != undefined? count[element] = count[element] + 1: count[element] = 1
-    });
-    console.log((count));
+    let charOccurrences = this.countOccurrences(string.split(""));
+    console.log(charOccurrences)
+    let occurrencesCounter = this.countOccurrences(Object.values(charOccurrences)) //count the chars with the same occurrences
+    console.log((occurrencesCounter));
     valid = false;
-    switch (Object.values(count).length ) {
+    switch (Object.values(occurrencesCounter).length ) {
       case 1: {
         valid = true;
         break
       };
-      case 2: {
-        if (Object.values(count)[0] == 1 || Object.values(count)[1]==1) {
-          let diff = Math.abs(Object.keys(count)[0] - Object.keys(count)[1]);
-          console.log(Object.keys(count)[0], Object.keys(count)[1]);
-          if ( diff == 1 ) {
-            valid = true
+      case 2: { 
+        //because could to remove one char only
+        // if (Object.values(occurrencesCounter)[0] == 1 || Object.values(occurrencesCounter)[1]==1) {
+        //   //char with unique occurrence
+        //   if ( Object.keys(occurrencesCounter)[0] == 1 || Object.keys(occurrencesCounter)[1] == 1) {
+        //     valid = true;
+        //   } else {
+        //     let toRemove
+        //     if (Object.keys(occurrencesCounter)[0] < Object.keys(occurrencesCounter)[1] ) {  }
+        //     let diff = Math.abs(Object.keys(occurrencesCounter)[0] - Object.keys(occurrencesCounter)[1]);
+        //     // console.log(Object.keys(occurrencesCounter)[0], Object.keys(occurrencesCounter)[1]);
+        //     if ( diff == 1) valid = true;
+        //   }
+        // }
+        // break
+        let toRemove
+        let equalOccurrenceValue 
+        if (Object.values(occurrencesCounter)[0] == 1) { 
+          toRemove = Object.keys(occurrencesCounter)[0]; 
+          equalOccurrenceValue = Object.keys(occurrencesCounter)[1];}
+        else {
+          if (Object.values(occurrencesCounter)[1] == 1) { 
+            toRemove = Object.keys(occurrencesCounter)[1];
+            equalOccurrenceValue = Object.keys(occurrencesCounter)[0];
           }
+          else break
         }
-        break
+        if (toRemove == 1 || toRemove - 1 == equalOccurrenceValue) {
+          valid = true;
+        }
       }
-
     }
-
     this.setState({valid: valid})
   }
 
@@ -67,10 +84,10 @@ class App extends React.Component {
   render() {
     return (
       <div className="App d-flex justify-content-center align-items-center">
-        <div className="jumbotron">
+        <div className="jumbotron shadow">
           <h5>Sherlock and the valid string</h5>
-          <p>Se verificara que la cadena sea valida</p>
-          <form className="form-inline d-flex justify-content-center">
+          <p>The string is valid if chars occurs same quantity or removing one char it has the same occurrences.</p>
+          <form className="form-inline d-flex justify-content-center mt-5" onSubmit={(e) => e.preventDefault()}>
             <div className="form-group mb-2">
 
               <input className="form-control" id="value" placeholder="Enter string..." onChange={this.handlerChange} ></input>
